7d5e6dd82d4845c75afa9240797ae8a9b4eee298
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-11-05 18:41:35 +0100

    Add a form to the body html for adding task to the db

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,3 +9,23 @@ Template.body.helpers({
         return Tasks.find({});
     },
 });
+
+Template.body.events({
+    'submit .new-task'(event) {
+        // prevent defaut browser events such as form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // Insert a task into the collection
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // This retrieves the current time
+        });
+
+        // Clear form
+        target.text.value = '';
+    },
+});
