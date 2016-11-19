16d59ab02249142533fc294fb2d33337024ebefe
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-11-19 22:01:36 +0100

    Publish tasks that is to be returned to the client from the server

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,34 +1,51 @@
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
-import { check } from 'meteor/check';
+import {
+  Meteor
+} from 'meteor/meteor';
+import {
+  Mongo
+} from 'meteor/mongo';
+import {
+  check
+} from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  // This code runs on the server alone
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find();
+  });
+}
+
 Meteor.methods({
-    'tasks.insert'(text) {
-        check(text, String);
+  'tasks.insert' (text) {
+    check(text, String);
 
-        // Make sure the user is logged before a task can be inserted
-        if(! this.userId) {
-            throw new Meteor.Error('not-authorized');
-        }
+    // Make sure the user is logged before a task can be inserted
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
 
-        Tasks.insert({
-            text,
-            createdAt: new Date(),
-            owner: this.userId,
-            username: Meteor.users.findOne(this.userId).username,
-        });
-    },
-    'tasks.remove'(taskId) {
-        check(taskId, String);
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: this.userId,
+      username: Meteor.users.findOne(this.userId).username,
+    });
+  },
+  'tasks.remove' (taskId) {
+    check(taskId, String);
 
-        Tasks.remove(taskId);
-    },
-    'tasks.setChecked'(taskId, setChecked) {
-        check(taskId, String);
-        check(setChecked, Boolean);
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked' (taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
 
-        Tasks.update(taskId, { $set: { checked: setChecked }});
-    },
+    Tasks.update(taskId, {
+      $set: {
+        checked: setChecked
+      }
+    });
+  },
 });
