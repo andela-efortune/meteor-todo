1403c25c5679b225c9df9abc97005e0bd07465e1
fortune ikechukwu ekeruo <legalmody@gmail.com>
2016-11-21 16:36:22 +0100

    Prepare the database for each test

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -2,13 +2,30 @@
 
 
 import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+
+import { Tasks } from './tasks.js';
 
 if (Meteor.isServer) {
     describe('Tasks', () => {
         describe('methods', () => {
+            const userId = Random.id();
+            let taskId;
+
+            beforeEach(() => {
+                Tasks.remove({});
+                taskId = Tasks.insert({
+                    test: 'test task',
+                    createdAt: new Date(),
+                    owner: userId,
+                    username: 'tmeasday',
+                });
+            });
+
             it('can delete owned task', () => {
 
             });
         });
     });
 }
+
